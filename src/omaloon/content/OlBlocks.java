c32f7433ef600e204928103003b246d59732f7e4
= <uujuju41@gmail.com>
2023-12-31 17:51:24 +0200

    thingy

--- src/omaloon/content/OlBlocks.java
+++ src/omaloon/content/OlBlocks.java
@@ -3,10 +3,12 @@ package omaloon.content;
 import omaloon.content.blocks.*;
 
 public class OlBlocks {
+
     public static void load(){
         OlEnvironmentBlocks.load();
         OlStorageBlocks.load();
         OlDistributionBlocks.load();
         OlPowerBlocks.load();
     }
+
 }
