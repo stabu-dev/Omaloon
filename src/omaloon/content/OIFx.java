2d7c490efd82d038dffdc55bb1f8a73b1eb7ddf4
pankiwi <arroliga.andrz1@gmail.com>
2024-01-24 06:07:59 +0200

    fixed asycn? hail stone maybe

---
+++ src/omaloon/content/OIFx.java
@@ -0,0 +1,85 @@
+package omaloon.content;
+
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
+import arc.graphics.g2d.Lines;
+import arc.math.Angles;
+import arc.math.Interp;
+import arc.math.Mathf;
+import arc.math.Rand;
+import arc.util.Tmp;
+import mindustry.entities.Effect;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
+import mindustry.graphics.Pal;
+import omaloon.entities.bullet.HailStoneBulletType;
+
+import static arc.graphics.Color.valueOf;
+import static arc.graphics.g2d.Draw.alpha;
+import static arc.graphics.g2d.Draw.color;
+import static arc.graphics.g2d.Lines.lineAngle;
+import static arc.graphics.g2d.Lines.stroke;
+import static arc.math.Angles.randLenVectors;
+
+public class OIFx {
+    public static final Rand rand = new Rand();
+    public static Effect
+
+    fellStone = new Effect(120f, e -> {
+        if(!(e.data instanceof HailStoneBulletType.HailStoneData data)) return;
+
+        Tmp.v2.trns(Mathf.randomSeed(e.id) * 360, data.fallTime/2 + Mathf.randomSeed(e.id + 1) * data.fallTime);
+        float scl = Interp.bounceIn.apply(e.fout() - 0.3f);
+        float rot = Tmp.v2.angle();
+        float x = e.x + (Tmp.v2.x * e.finpow()), y = e.y + (Tmp.v2.y * e.finpow());
+
+        Draw.z(Layer.power + 0.1f);
+        Draw.scl();
+        Drawf.shadow(data.region, x, y, rot);
+
+        Draw.z(Layer.power + 0.2f);
+        Draw.color(e.color);
+        Draw.scl();
+        Draw.rect(data.region, x, y + (scl * data.fallTime/2), rot);
+    }),
+
+    staticStone = new Effect(250f, e -> {
+        if(!(e.data instanceof HailStoneBulletType.HailStoneData data)) return;
+
+        Draw.z(Layer.power + 0.1f);
+        Draw.alpha(e.fout());
+        Draw.color(e.color);
+        Draw.rect(data.region, e.x, e.y, Mathf.randomSeed(e.id) * 360);
+    }),
+
+    explosionStone = new Effect(60f, e -> {
+        Angles.randLenVectors(e.id, 12, e.fin() * 50f, (x, y) -> {
+            float elevation = Interp.bounceIn.apply(e.fout() - 0.3f) * (Mathf.randomSeed((int) Angles.angle(x, y), 30f, 60f));
+
+            Draw.z(Layer.power + 0.1f);
+            Draw.color(Pal.shadow);
+            Fill.circle(e.x + x, e.y + y, 12f);
+
+            Draw.z(Layer.power + 0.2f);
+            Draw.color(e.color);
+            Fill.circle(e.x + x, e.y + y + elevation, 12f);
+        });
+
+    }),
+
+    bigExplosionStone = new Effect(80f, e -> {
+        Angles.randLenVectors(e.id, 22, e.fin() * 50f, (x, y) -> {
+            float elevation = Interp.bounceIn.apply(e.fout() - 0.3f) * (Mathf.randomSeed((int) Angles.angle(x, y), 30f, 60f));
+
+            Draw.z(Layer.power + 0.1f);
+            Draw.color(Pal.shadow);
+            Fill.circle(e.x + x, e.y + y, 12f);
+
+            Draw.z(Layer.power + 0.2f);
+            Draw.color(e.color);
+            Fill.circle(e.x + x, e.y + y + elevation, 12f);
+        });
+
+    });
+}
