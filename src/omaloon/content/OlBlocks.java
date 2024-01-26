fcdffb9742482133f1521e40022a37d0cc24cd45
= <uujuju41@gmail.com>
2024-01-26 16:05:41 +0200

    turbulence (less ram usage)

--- src/omaloon/content/OlBlocks.java
+++ src/omaloon/content/OlBlocks.java
@@ -1,13 +1,14 @@
 package omaloon.content;
 
-import omaloon.content.blocks.*;;
+import omaloon.content.blocks.*;
 
 public class OlBlocks {
+
     public static void load(){
         OlEnvironmentBlocks.load();
         OlStorageBlocks.load();
-        OlProductionBlocks.load();
         OlDistributionBlocks.load();
         OlPowerBlocks.load();
     }
+
 }
--- src/omaloon/graphics/Drawm.java
+++ src/omaloon/graphics/Drawm.java
@@ -4,7 +4,6 @@ import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arclibrary.graphics.*;
 import mindustry.graphics.*;
-
 public class Drawm{
 
     public static void dashPoly(float partSize, Color color, float... cords){
@@ -17,11 +16,5 @@ public class Drawm{
 
     public static void dashPoly(Color color, float... cords){
         dashPoly(10, color, cords);
-    }
-
-    public static void shadow(TextureRegion region, float x, float y, float rotation, float alpha){
-        Draw.color(Pal.shadow, alpha);
-        Draw.rect(region, x, y, rotation);
-        Draw.color();
     }
 }
