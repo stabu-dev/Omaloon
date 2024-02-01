d05b12e16d1cd65baaa7bba11e9f5870af214a98
= <uujuju41@gmail.com>
2024-02-01 06:13:32 +0200

    does a funni

--- src/omaloon/content/OlItems.java
+++ src/omaloon/content/OlItems.java
@@ -1,11 +1,11 @@
 package omaloon.content;
 
-import arc.graphics.*;
+import arc.graphics.Color;
 import mindustry.type.*;
 
 public class OlItems {
     public static Item
-            cobalt, carborundum,
+            cobalt,
 
     end;
 
@@ -13,11 +13,6 @@ public class OlItems {
         cobalt = new Item("cobalt", Color.valueOf("85939d")){{
             hardness = 1;
             cost = 0.5f;
-            alwaysUnlocked = true;
-        }};
-
-        carborundum = new Item("carborundum", Color.valueOf("614a7f")){{
-            cost = 0.7f;
             alwaysUnlocked = true;
         }};
     }
