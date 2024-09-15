b02d1dc14c87a43994c4c6cfaecb79c54f853381
xstabux <buksta@outlook.com>
2024-09-16 00:45:18 +0300

    no splashes when no rain

--- src/omaloon/OmaloonMod.java
+++ src/omaloon/OmaloonMod.java
@@ -40,7 +40,6 @@ public class OmaloonMod extends Mod{
                 cliffFragment.build(Vars.ui.hudGroup);
                 OlBinding.load();
             }
-            Vars.maps.all().removeAll(map -> map.mod != null && map.mod.name.equals("omaloon"));
         });
 
         if(!Vars.headless) editorListener = new EditorListener();
--- src/omaloon/content/OlStatusEffects.java
+++ src/omaloon/content/OlStatusEffects.java
@@ -5,7 +5,6 @@ import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
 import mindustry.*;
-import mindustry.core.*;
 import mindustry.entities.*;
 import mindustry.game.*;
 import mindustry.graphics.*;
@@ -52,22 +51,22 @@ public class OlStatusEffects {
         filledWithWater = new StatusEffect("filled-with-water"){{
         }
         @Override
-        public boolean isHidden(){return Vars.state.getState() != GameState.State.menu;}
+        public boolean isHidden(){return Vars.state.isPlaying();}
         };
         filledWithGlacium = new StatusEffect("filled-with-glacium"){{
         }
         @Override
-        public boolean isHidden(){return Vars.state.getState() != GameState.State.menu;}
+        public boolean isHidden(){return Vars.state.isPlaying();}
         };
         filledWithSlag = new StatusEffect("filled-with-slag"){{
         }
         @Override
-        public boolean isHidden(){return Vars.state.getState() != GameState.State.menu;}
+        public boolean isHidden(){return Vars.state.isPlaying();}
         };
         filledWithOil = new StatusEffect("filled-with-oil"){{
         }
         @Override
-        public boolean isHidden(){return Vars.state.getState() != GameState.State.menu;}
+        public boolean isHidden(){return Vars.state.isPlaying();}
         };
     }
 }
