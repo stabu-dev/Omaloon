1903d8d37373838878aa5558665a96fe14654806
= <uujuju41@gmail.com>
2024-11-11 01:30:50 +0200

    end dialog

--- src/omaloon/content/OlSectorPresets.java
+++ src/omaloon/content/OlSectorPresets.java
@@ -49,9 +49,16 @@ public class OlSectorPresets {
 		}) {{
 			captureWave = 15;
 		}};
-		deadValley = new ExtraSectorPreset("dead_valley", OlPlanets.glasmore, 660) {{
+		/*deadValley = new ExtraSectorPreset("dead_valley", OlPlanets.glasmore, 660, () -> {
+			if (getFlag("addweather", true)) {
+				Vars.state.rules.weather.clear();
+				Vars.state.rules.weather.add(
+					new WeatherEntry(OlWeathers.wind, 0.5f * toMinutes, 2.5f * toMinutes, 5f * toMinutes, 10f * toMinutes)
+				);
+			}
+		}) {{
 			captureWave = 20;
 			difficulty = 3;
-		}};
+		}};*/
 	}
 }
