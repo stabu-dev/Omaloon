ebb0e38e5e44eafd35d186f8d3923928061f7432
Zelaux <58040045+Zelaux@users.noreply.github.com>
2025-03-03 10:24:25 +0200

    Added OlRenderer

--- src/omaloon/OmaloonMod.java
+++ src/omaloon/OmaloonMod.java
@@ -3,11 +3,10 @@ package omaloon;
 import arc.*;
 import arc.scene.actions.*;
 import arc.util.*;
+import arclibrary.settings.*;
 import mindustry.*;
-import mindustry.content.TechTree.*;
 import mindustry.game.*;
 import mindustry.mod.*;
-import mindustry.type.*;
 import ol.gen.*;
 import omaloon.content.*;
 import omaloon.core.*;
@@ -39,9 +38,11 @@ public class OmaloonMod extends Mod{
     public static OlControl control;
     public static EditorListener editorListener;
     public static OlNetClient netClient;
+    public static OlRenderer renderer;
 
     public OmaloonMod(){
         OlCall.registerPackets();
+        SettingKeyGroup.defaultGroup.eachKey(SettingKey::setDefault);
         new OlDelayedItemTransfer();
 
         appListener(new ApplicationListener(){
@@ -54,11 +55,12 @@ public class OmaloonMod extends Mod{
             editorListener = appListener(new EditorListener());
             ui = appListener(new OlUI());
             control = appListener(new OlControl());
-            netClient= RelatedApplicationListener.register(
+            netClient = RelatedApplicationListener.register(
                 new OlNetClient(),
                 RelativeOrder.before,
                 Vars.netClient
             );
+            renderer = appListener(new OlRenderer());
         }
 
         OlVars.onClientLoad(() -> {
@@ -94,20 +96,6 @@ public class OmaloonMod extends Mod{
 
     public static void olLog(String string, Object... args){
         Log.infoTag("omaloon", Strings.format(string, args));
-    }
-
-    public static void resetSaves(Planet planet){
-        planet.sectors.each(sector -> {
-            if(!sector.hasSave()) return;
-            sector.save.delete();
-            sector.save = null;
-        });
-    }
-
-    public static void resetTree(TechNode root){
-        root.reset();
-        root.content.clearUnlock();
-        root.children.each(OmaloonMod::resetTree);
     }
 
     @Override
