291a675e07c573e733f59d1567bbe2b0cf9a9309
= <uujuju41@gmail.com>
2025-03-16 19:52:38 +0200

    a

--- src/omaloon/ui/OlBinding.java
+++ src/omaloon/ui/OlBinding.java
@@ -3,12 +3,21 @@ package omaloon.ui;
 import arc.KeyBinds.*;
 import arc.input.InputDevice.*;
 import arc.input.*;
+import arc.struct.*;
+import org.jetbrains.annotations.*;
 
 public enum OlBinding implements KeyBind{
     shaped_env_placer(KeyCode.o, "omaloon-editor"),
     switchDebugDraw(KeyCode.f12, "omaloon-debug-draw"),
     cliff_placer(KeyCode.p, "omaloon-editor");
 
+    public static final @NotNull ObjectMap<String, Seq<OlBinding>> categoryMap;
+    static {
+        categoryMap = new ObjectMap<>();
+        for(OlBinding bind : OlBinding.values()){
+            categoryMap.get(bind.category(),Seq::new).add(bind);
+        }
+    }
     private final KeybindValue defaultValue;
     private final String category;
 
