6a716eb02183574cd436e7ef07c8da349caf98c0
= <uujuju41@gmail.com>
2025-04-17 10:06:16 -0300

    mmmmmmmmm

--- src/omaloon/world/blocks/distribution/TubeGate.java
+++ src/omaloon/world/blocks/distribution/TubeGate.java
@@ -1,12 +1,54 @@
 package omaloon.world.blocks.distribution;
 
+import arc.graphics.g2d.*;
+import arc.struct.*;
+import arc.util.io.*;
+import mindustry.*;
+import mindustry.type.*;
 import mindustry.world.blocks.distribution.*;
+import mindustry.world.draw.*;
+import omaloon.annotations.*;
 
 public class TubeGate extends OverflowGate{
+		public DrawBlock drawer = new DrawDefault();
+
+		@Load("@-rotator")
+		public TextureRegion rotorRegion;
+
     public TubeGate(String name){
         super(name);
     }
 
-    public class TubeGateBuild extends OverflowGateBuild{
+		@Override
+		public void load(){
+				super.load();
+				drawer.load(this);
+		}
+
+		public class TubeGateBuild extends OverflowGateBuild{
+        public ObjectIntMap<Item> directionalItems = new ObjectIntMap<>();
+
+        @Override
+        public void read(Reads read, byte revision){
+            super.read(read, revision);
+
+            short size = read.s();
+            for(int i = 0; i < size; i++){
+                int item = read.i();
+                int build = read.i();
+                directionalItems.put(Vars.content.item(item), build);
+            }
+        }
+
+        @Override
+        public void write(Writes write){
+            super.write(write);
+
+            write.s(directionalItems.size);
+            directionalItems.keys().toArray().each(item -> {
+                write.i(item.id);
+                write.i(directionalItems.get(item));
+            });
+        }
     }
 }
