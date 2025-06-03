7b26f2fcd10dce4b3c9b16b81edc4eb1d70da345
stabu-dev <buksta@outlook.com>
2025-06-04 01:51:56 +0300

    start splash re-introduction

--- main/src/omaloon/OmaloonMod.java
+++ main/src/omaloon/OmaloonMod.java
@@ -1,43 +1,49 @@
 package omaloon;
 
 import arc.*;
-
+import arc.util.*;
 import mindustry.ctype.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
-
+import mindustry.mod.Mods.*;
 import omaloon.annotations.Annotations.*;
+import omaloon.core.*;
 import omaloon.gen.*;
+import omaloon.ui.*;
+import omaloon.ui.dialogs.*;
 
+import static arc.Core.app;
 import static mindustry.Vars.*;
 
-/**
- * The Omaloon's main class. Contains static references to other modules.
- * @author stabu_
- */
-@LoadRegs("error")// Need this temporarily, so the class gets generated.
+@LoadRegs("error")
 @EnsureLoad
 public class OmaloonMod extends Mod{
     public static boolean tools = false;
+    protected static LoadedMod mod;
 
-    /** Default constructor for Mindustry mod loader to instantiate. */
     public OmaloonMod(){
         this(false);
     }
 
-    /**
-     * Constructs the Omaloon and binds some functionality to the game under certain circumstances.
-     * @param tools Whether the Omaloon is in an asset-processing context.
-     */
     public OmaloonMod(boolean tools){
         OmaloonMod.tools = tools;
 
-        if(!headless){
-            Events.on(FileTreeInitEvent.class, e -> Core.app.post(OlSounds::load));
+        Events.on(ClientLoadEvent.class, e -> {
+            OlIcons.load();
+            OlSettings.load();
 
-            Events.on(ClientLoadEvent.class, e -> {
-                //upon startup
+            app.post(() -> {
+                if(Core.scene != null && Core.scene.root != null){
+                    StartSplash.build();
+                    StartSplash.show();
+                }else{
+                    Log.err("OmaloonMod: Scene not initialized during ClientLoadEvent. StartSplash cannot be shown.");
+                }
             });
+        });
+
+        if(!headless){
+            Events.on(FileTreeInitEvent.class, e -> app.post(OlSounds::load));
         }
 
         Events.on(ContentInitEvent.class, e -> {
@@ -50,6 +56,8 @@ public class OmaloonMod extends Mod{
                 });
             }
         });
+
+        app.post(() -> mod = mods.getMod(OmaloonMod.class));
     }
 
     @Override
@@ -58,11 +66,14 @@ public class OmaloonMod extends Mod{
 
     @Override
     public void loadContent(){
-        //below has to be done after all things are loaded.
         OlEntityMapping.init();
     }
 
     public static boolean isOmaloon(Content content){
         return content.minfo.mod != null && content.minfo.mod.name.equals("omaloon");
+    }
+
+    public static LoadedMod mod(){
+        return mod;
     }
 }
