69f2570861e7578f118b2c1dd3773bd16301de7d
= <uujuju41@gmail.com>
2025-06-09 03:13:27 +0300

    can't test this bust it might fix the issue with the mobile splash screen

--- main/src/omaloon/OmaloonMod.java
+++ main/src/omaloon/OmaloonMod.java
@@ -38,7 +38,7 @@ public class OmaloonMod extends Mod{
 
         if(!headless){
             app.post(() -> {
-                if(SplashDrawer.isEnabled()) new SplashDrawer(mods.getMod(OmaloonMod.class));
+                if(SplashDrawer.isEnabled()) Core.app.addListener(new SplashDrawer(mods.getMod(OmaloonMod.class)));
                 OlSounds.load();
             });
         }
