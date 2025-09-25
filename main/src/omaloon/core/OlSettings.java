b51fff015a4df46d0f0cc5c069c06e8b5a59310a
= <uujuju41@gmail.com>
2025-09-26 02:30:37 +0300

    hints

--- main/src/omaloon/core/OlSettings.java
+++ main/src/omaloon/core/OlSettings.java
@@ -1,10 +1,12 @@
 package omaloon.core;
 
+import arc.func.*;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
 import mindustry.gen.*;
 import mindustry.ui.dialogs.*;
 import mindustry.ui.dialogs.SettingsMenuDialog.SettingsTable.*;
+import omaloon.ui.*;
 
 import static arc.Core.*;
 import static mindustry.Vars.ui;
@@ -44,6 +46,15 @@ public class OlSettings{
             table.checkPref("omaloon-check-updates", false);
             table.checkPref("omaloon-override-stats", true);
 
+            table.pref(new ButtonSetting("reset-hints", () -> {
+                Button b = new Button(Tex.buttonSideLeft, Tex.buttonSideLeftDown);
+
+                b.add("@setting.omaloon-reset-hints").margin(10);
+                b.clicked(EventHints::reset);
+
+                return b;
+            }));
+
             // discord link
             table.pref(new TableSetting("discord-link", new Table(c -> {
                 c.bottom().right().button(
@@ -77,6 +88,21 @@ public class OlSettings{
         @Override
         public void add(SettingsMenuDialog.SettingsTable table){
             table.add(t).fillX().row();
+        }
+    }
+
+    /** A setting that contains a button. */
+    public static class ButtonSetting extends Setting{
+        public Button button;
+
+        public ButtonSetting(String name, Prov<Button> button){
+            super(name);
+            this.button = button.get();
+        }
+
+        @Override
+        public void add(SettingsMenuDialog.SettingsTable table){
+            table.add(button).fillX().row();
         }
     }
 }
---
+++ main/src/omaloon/ui/EventHints.java
@@ -0,0 +1,111 @@
+package omaloon.ui;
+
+import arc.*;
+import arc.func.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.ui.fragments.HintsFragment.*;
+import omaloon.content.blocks.*;
+import omaloon.world.blocks.defense.*;
+import omaloon.world.blocks.distribution.*;
+import omaloon.world.interfaces.*;
+
+public enum EventHints implements Hint{
+    air(
+        () -> false,
+        () -> Vars.state.rules.defaultTeam.data().buildings.contains(b -> b instanceof HasPressure p && p.pressureConfig().fluidCapacity > 8f)
+    ),
+    drills(
+        () -> false,
+        () -> Vars.control.input.block == OlProductionBlocks.hammerDrill
+    ),
+    pump_chaining(
+        () -> false,
+        () -> Vars.control.input.block instanceof PressureLiquidPump &&
+        Vars.state.rules.defaultTeam.data().buildings.contains(b -> b.block instanceof PressureLiquidPump)
+    ),
+    press(
+        () -> false,
+        () -> Vars.control.input.block == OlCraftingBlocks.compositePress
+    ),
+    shelter(
+        () -> false,
+        () -> Vars.control.input.block instanceof Shelter
+    );
+
+    final Boolp complete;
+    Boolp shown = () -> true;
+    EventHints[] requirements;
+
+    int visibility = visibleAll;
+    boolean cached, finished;
+
+    static final String prefix = "omaloon-";
+
+    public static void addHints(){
+        Vars.ui.hints.hints.add(Seq.with(EventHints.values()).removeAll(
+            hint -> Core.settings.getBool(prefix + hint.name() + "-hint-done", false)
+        ));
+    }
+
+    EventHints(Boolp complete){
+        this.complete = complete;
+    }
+
+    EventHints(Boolp complete, Boolp shown){
+        this(complete);
+        this.shown = shown;
+    }
+
+    EventHints(Boolp complete, Boolp shown, EventHints... requirements){
+        this(complete, shown);
+        this.requirements = requirements;
+    }
+
+    @Override
+    public boolean complete(){
+        return complete.get();
+    }
+
+    @Override
+    public void finish(){
+        Core.settings.put(prefix + name() + "-hint-done", finished = true);
+    }
+
+    @Override
+    public boolean finished(){
+        if(!cached){
+            cached = true;
+            finished = Core.settings.getBool(prefix + name() + "-hint-done", false);
+        }
+        return finished;
+    }
+
+    @Override
+    public int order(){
+        return ordinal();
+    }
+
+    public static void reset(){
+        for(EventHints hint : values()){
+            Core.settings.put(prefix + hint.name() + "-hint-done", hint.finished = false);
+        }
+        addHints();
+    }
+
+    @Override
+    public boolean show(){
+        return shown.get() && (requirements == null || (requirements.length == 0 || !Structs.contains(requirements, d -> !d.finished())));
+    }
+
+    @Override
+    public String text(){
+        return Core.bundle.get("hint." + prefix + name(), "Missing bundle for hint: hint." + prefix + name());
+    }
+
+    @Override
+    public boolean valid(){
+        return (Vars.mobile && (visibility & visibleMobile) != 0) || (!Vars.mobile && (visibility & visibleDesktop) != 0);
+    }
+}
