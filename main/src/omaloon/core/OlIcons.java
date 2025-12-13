061ab46edd71a276372cce5871aa27389581c3bd
stabu_ <buksta@outlook.com>
2025-12-14 01:01:19 +0200

    good(?)

--- main/src/omaloon/core/OlIcons.java
+++ main/src/omaloon/core/OlIcons.java
@@ -9,9 +9,9 @@ public class OlIcons{
     public static TextureRegionDrawable olSettings, glasmore, purpura;
 
     public static void load(){
-        olSettings = atlas.getDrawable("omaloon-settings");
-        glasmore = atlas.getDrawable("omaloon-glasmore");
-        purpura = atlas.getDrawable("omaloon-purpura");
+        olSettings = atlas.getDrawable("omaloon-settings-ui");
+        glasmore = atlas.getDrawable("omaloon-glasmore-ui");
+        purpura = atlas.getDrawable("omaloon-purpura-ui");
         icons.put("omaloon-settings", olSettings);
         icons.put("glasmore", glasmore);
         icons.put("purpura", purpura);
