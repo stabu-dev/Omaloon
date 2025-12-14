933c797d06edbfd3eb4d7a1d1f14d1be7b0a3c0c
= <uujuju41@gmail.com>
2025-12-14 02:14:47 +0200

    spawn weather base class

---
+++ main/src/omaloon/type/SpawnWeather.java
@@ -0,0 +1,37 @@
+package omaloon.type;
+
+import arc.math.*;
+import mindustry.*;
+import mindustry.gen.*;
+import mindustry.type.*;
+
+public class SpawnWeather extends Weather{
+    public int spawns = 0;
+
+    public SpawnWeather(String name) {
+        super(name);
+    }
+
+    @Override
+    public boolean isHidden(){
+        return localizedName.equals(name) || hidden;
+    }
+
+    public boolean shouldSpawn(WeatherState state) {
+        return true;
+    }
+
+    public void spawn(WeatherState state, float x, float y) {
+    }
+
+    @Override
+    public void update(WeatherState state){
+        for(int spawn = 0; spawn < spawns; spawn++) {
+            if (shouldSpawn(state)) {
+                float rx = Mathf.random(0f, Vars.world.unitWidth());
+                float ry = Mathf.random(0f, Vars.world.unitHeight());
+                spawn(state, rx, ry);
+            }
+        }
+    }
+}
