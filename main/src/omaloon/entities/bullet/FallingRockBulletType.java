665fede3998d7683e903564b097bc6e562c02cd5
= <uujuju41@gmail.com>
2025-12-14 18:00:31 +0200

    now the rocks

---
+++ main/src/omaloon/entities/bullet/FallingRockBulletType.java
@@ -0,0 +1,85 @@
+package omaloon.entities.bullet;
+
+import arc.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+
+public class FallingRockBulletType extends BulletType{
+    public String name;
+
+    public float fallDistance = 0;
+
+    public int variants = 0;
+
+    public TextureRegion[] variantRegions;
+
+    public FallingRockBulletType(String name) {
+        this.name = name;
+        damage = speed = 0;
+        collides = false;
+    }
+
+    @Override
+    public void despawned(Bullet b){
+        if(despawnHit){
+            hit(b);
+        }else{
+            createUnits(b, b.x, b.y);
+        }
+
+        if(!fragOnHit){
+            createFrags(b, b.x, b.y);
+        }
+
+        despawnEffect.at(b.x, b.y, b.rotation(), hitColor, new RockData(){{
+            region = variantRegions[variant(b)];
+            bullet = (FallingRockBulletType)b.type;
+        }});
+        despawnSound.at(b);
+
+        Effect.shake(despawnShake, despawnShake, b);
+    }
+
+    @Override
+    public void draw(Bullet b){
+        super.draw(b);
+
+        float ox = b.x;
+        float oy = b.y + fallDistance * b.fout();
+
+        Draw.alpha(Mathf.clamp(b.fin() * 5f));
+        Draw.rect(variantRegions[variant(b)], ox, oy);
+
+        Draw.mixcol(Pal.shadow, 1f);
+        Draw.alpha(b.fin() * Pal.shadow.a);
+        Draw.rect(variantRegions[variant(b)], b.x, b.y);
+        Draw.mixcol();
+    }
+
+    @Override
+    public void load(){
+        super.load();
+
+        if(variants > 0){
+            variantRegions = new TextureRegion[variants];
+            for(int i = 0; i < variants; i++){
+                variantRegions[i] = Core.atlas.find(name + "-" + i);
+            }
+        }else{
+            variantRegions = new TextureRegion[]{Core.atlas.find(name)};
+        }
+    }
+
+    public int variant(Bullet b){
+        return Mathf.randomSeed(b.id, 0, Math.max(0, variantRegions.length - 1));
+    }
+
+    public static class RockData{
+        public TextureRegion region;
+        public FallingRockBulletType bullet;
+    }
+}
--- main/src/omaloon/world/draw/DrawWindTurbine.java
+++ main/src/omaloon/world/draw/DrawWindTurbine.java
@@ -17,7 +17,7 @@ import mindustry.world.draw.*;
 public class DrawWindTurbine extends DrawBlock{
     public String suffix = "-rotator";
     public float rotateSpeed = 1f, beamStroke = 1.6f, armLength = 11f;
-    public float minShadowOffset = -3.5f, shadowOffset = -16f, topOffset = -12f;
+    public float minShadowOffset = -4f, shadowOffset = -18f;
     public int shadowPrecision = 20;
 
     public TextureRegion rotatorRegion, topRegion, capRegion;
@@ -86,10 +86,11 @@ public class DrawWindTurbine extends DrawBlock{
 
         shadowBufferDrawCalls.add(() -> {
             Gl.clear(Gl.stencilBufferBit);
-            Draw.rect(capRegion, b.x + topOffset, b.y + topOffset);
+            float centerOffset = (shadowOffset + minShadowOffset) / 2f - 1f;
+            Draw.rect(capRegion, b.x + centerOffset, b.y + centerOffset);
 
             Lines.stroke(beamStroke);
-            Lines.line(b.x, b.y, b.x + topOffset, b.y + topOffset, false);
+            Lines.line(b.x, b.y, b.x + centerOffset, b.y + centerOffset, false);
 
             float bladeTopOffset = shadowOffset + 1.5f;
             float bladeBotOffset = minShadowOffset - 1.5f;
@@ -97,7 +98,7 @@ public class DrawWindTurbine extends DrawBlock{
 
             for(int s : Mathf.signs){
                 float a = ang + (s == 1 ? 0 : 180f);
-                float bx = b.x + topOffset, by = b.y + topOffset;
+                float bx = b.x + centerOffset, by = b.y + centerOffset;
 
                 Lines.line(bx, by, b.x + bladeTopOffset + Angles.trnsx(a, armLength), b.y + bladeTopOffset + Angles.trnsy(a, armLength));
                 Lines.line(bx, by, b.x + bladeBotOffset + Angles.trnsx(a, armLength), b.y + bladeBotOffset + Angles.trnsy(a, armLength));
@@ -119,6 +120,7 @@ public class DrawWindTurbine extends DrawBlock{
                 Draw.rect(rotatorRegion, b.x + off, b.y + off, totalProgress);
             }
         });
+
     }
 
     @Override
