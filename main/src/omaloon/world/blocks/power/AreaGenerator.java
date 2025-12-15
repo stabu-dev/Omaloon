6eb2b35aa519b75de6af2cb8b9f17667f926fda5
stabu_ <buksta@outlook.com>
2025-12-15 16:05:43 +0200

    one less way to bypass placement range

--- main/src/omaloon/world/blocks/power/AreaGenerator.java
+++ main/src/omaloon/world/blocks/power/AreaGenerator.java
@@ -14,6 +14,7 @@ import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.input.*;
 import mindustry.world.*;
+import mindustry.world.blocks.ConstructBlock.*;
 import mindustry.world.blocks.power.*;
 import mindustry.world.consumers.*;
 import mindustry.world.meta.*;
@@ -35,7 +36,15 @@ public class AreaGenerator extends ConsumeGenerator{
 
     @Override
     public boolean canPlaceOn(Tile tile, Team team, int rotation){
-        return range == 0 || !checkNearby(tile.x, tile.y, ntile -> ntile != null && ntile.block() == this);
+        if(range == 0) return true;
+
+        return !checkNearby(tile.x, tile.y, ntile -> {
+            if(ntile == null) return false;
+
+            if(ntile.block() == this) return true;
+
+            return ntile.build instanceof ConstructBuild cb && cb.current == this;
+        });
     }
 
     @Override
