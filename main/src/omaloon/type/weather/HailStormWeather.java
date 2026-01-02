15f50558c3e04944d52716702823cdd16529fd08
= <uujuju41@gmail.com>
2026-01-02 00:00:34 -0300

    possibly making certain lower chance bullets not spawn with less intensity untested, no idea if it'll work

--- main/src/omaloon/type/weather/HailStormWeather.java
+++ main/src/omaloon/type/weather/HailStormWeather.java
@@ -113,7 +113,7 @@ public class HailStormWeather extends SpawnWeather{
     @Override
     public void spawn(WeatherState state, float x, float y){
         if (Vars.net.client()) return;
-        float intensity = Math.max(minIntensity, Mathf.random(1f));
+        float intensity = Mathf.lerp(1f, Math.max(minIntensity, Mathf.random(1f)), state.intensity);
 
         threshold = Float.NEGATIVE_INFINITY;
         picked = null;
