d81693e0c73ffb9cc536795348d227cd0a54dd3a
stabu_ <buksta@outlook.com>
2026-02-24 23:28:27 +0200

    walker (no manual boost)

--- main/src/omaloon/entities/comp/FloatMechComp.java
+++ main/src/omaloon/entities/comp/FloatMechComp.java
@@ -1,14 +1,26 @@
 package omaloon.entities.comp;
 
+import arc.math.*;
 import mindustry.entities.*;
 import mindustry.gen.*;
+import mindustry.type.*;
 import omaloon.annotations.Annotations.*;
 
 @EntityComponent
-abstract class FloatMechComp implements Unitc, Mechc {
+abstract class FloatMechComp implements Unitc, Mechc{
+    @Import
+    UnitType type;
+    @Import
+    float elevation;
+
+    @Override
+    public void update(){
+        elevation = Mathf.approachDelta(elevation, onSolid() ? 1f : 0f, type.riseSpeed);
+    }
+
     @Replace(1)
     @Override
-    public EntityCollisions.SolidPred solidity() {
+    public EntityCollisions.SolidPred solidity(){
         return null;
     }
 }
