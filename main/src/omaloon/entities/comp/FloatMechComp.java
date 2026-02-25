b5f8131f444d6a9afe7d4d9fbd47b002d2c8faaf
stabu_ <buksta@outlook.com>
2026-02-25 15:41:41 +0200

    walker (boost over build plans)

--- main/src/omaloon/entities/comp/FloatMechComp.java
+++ main/src/omaloon/entities/comp/FloatMechComp.java
@@ -1,21 +1,34 @@
 package omaloon.entities.comp;
-
 import arc.math.*;
+import arc.util.*;
 import mindustry.entities.*;
+import mindustry.entities.units.*;
 import mindustry.gen.*;
 import mindustry.type.*;
 import omaloon.annotations.Annotations.*;
 
 @EntityComponent
-abstract class FloatMechComp implements Unitc, Mechc{
+abstract class FloatMechComp implements Unitc, Mechc {
     @Import
     UnitType type;
     @Import
     float elevation;
-
     @Override
     public void update(){
-        elevation = Mathf.approachDelta(elevation, onSolid() ? 1f : 0f, type.riseSpeed);
+        elevation = Mathf.approachDelta(elevation, onSolid() || isUnderBuildPlan() ? 1f : 0f, type.riseSpeed);
+    }
+
+    boolean isUnderBuildPlan(){
+        hitboxTile(Tmp.r1);
+        for(Unit unit : Groups.unit){
+            for(BuildPlan plan : unit.plans()){
+                if(plan.breaking || plan.block == null) continue;
+                if(!plan.block.solid && !plan.block.solidifes) continue;
+                plan.hitbox(Tmp.r2);
+                if(Tmp.r1.overlaps(Tmp.r2)) return true;
+            }
+        }
+        return false;
     }
 
     @Replace(1)
