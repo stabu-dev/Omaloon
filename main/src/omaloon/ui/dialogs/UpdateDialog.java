c7d6877514684898ad154a76a5a8ce13b1664302
Liz <j4224253@gmail.com>
2026-03-07 00:01:34 -0300

    code style funsies

--- main/src/omaloon/ui/dialogs/UpdateDialog.java
+++ main/src/omaloon/ui/dialogs/UpdateDialog.java
@@ -21,7 +21,7 @@ import static mindustry.Vars.*;
  * 
  * @author stabu_
  */
-public class UpdateDialog {
+public class UpdateDialog{
     public static final String repo = "stabu-dev/Omaloon";
 
     public static Mods.LoadedMod mod = Vars.mods.locateMod("omaloon");
@@ -32,23 +32,22 @@ public class UpdateDialog {
     public static String download;
 
     /** Checks for updates and shows a dialog if a new version is available. */
-    public static void check() {
-        if (!Core.settings.getBool("omaloon-check-updates"))
-            return;
+    public static void check(){
+        if(!Core.settings.getBool("omaloon-check-updates")) return;
         Http.get(url, res -> {
             Jval json = Jval.read(res.getResultAsString());
             String latest = json.getString("tag_name").substring(1);
             download = json.get("assets").asArray().get(0).getString("browser_download_url");
 
-            if (!latest.equals(mod.meta.version)) {
+            if(!latest.equals(mod.meta.version)){
                 BaseDialog dialog = new BaseDialog("@dialog.omaloon-updater.title");
 
                 dialog.cont.add(bundle.format("dialog.omaloon-updater", mod.meta.version, latest))
-                        .width(mobile ? 400f : 500f)
-                        .wrap()
-                        .pad(4f)
-                        .get()
-                        .setAlignment(Align.center, Align.center);
+                .width(mobile ? 400f : 500f)
+                .wrap()
+                .pad(4f)
+                .get()
+                .setAlignment(Align.center, Align.center);
 
                 dialog.buttons.defaults().size(200f, 54f).pad(2f);
 
@@ -56,7 +55,7 @@ public class UpdateDialog {
                 dialog.buttons.button("@button.omaloon-ignore", Icon.cancel, dialog::hide);
 
                 dialog.buttons.button("@button.omaloon-updater-show-changes", Icon.link, () -> {
-                    if (!Core.app.openURI(changes)) {
+                    if(!Core.app.openURI(changes)){
                         ui.showInfoFade("@linkfail");
                         Core.app.setClipboardText(changes);
                     }
@@ -72,14 +71,14 @@ public class UpdateDialog {
     }
 
     /** Closes the old mod jar and starts the download of the new version. */
-    public static void update() {
-        try {
-            if (mod.loader instanceof URLClassLoader cl) {
+    public static void update(){
+        try{
+            if(mod.loader instanceof URLClassLoader cl){
                 cl.close();
             }
 
             mod.loader = null;
-        } catch (Throwable ignored) {
+        }catch(Throwable ignored){
         }
 
         ui.loadfrag.show("@downloading");
@@ -93,22 +92,23 @@ public class UpdateDialog {
      * 
      * @param res The HTTP response containing the downloaded file.
      */
-    public static void handle(Http.HttpResponse res) {
-        try {
+    public static void handle(Http.HttpResponse res){
+        try{
             Fi file = tmpDirectory.child(repo.replace("/", "") + ".zip");
             Streams.copyProgress(
-                    res.getResultAsStream(),
-                    file.write(false),
-                    res.getContentLength(),
-                    4096,
-                    p -> progress = p);
+            res.getResultAsStream(),
+            file.write(false),
+            res.getContentLength(),
+            4096,
+            p -> progress = p
+            );
 
             mods.importMod(file).setRepo(repo);
             file.delete();
 
             app.post(ui.loadfrag::hide);
             ui.showInfoOnHidden("@mods.reloadexit", app::exit);
-        } catch (Throwable ignored) {
+        }catch(Throwable ignored){
         }
     }
 }
