2139b826df57792fc95ba169c6abed07178373d0
Liz <j4224253@gmail.com>
2026-03-16 03:44:34 +0200

    darkness drawing on world load

--- main/src/omaloon/graphics/OlRenderer.java
+++ main/src/omaloon/graphics/OlRenderer.java
@@ -1,6 +1,13 @@
 package omaloon.graphics;
 
+import arc.*;
+import arc.graphics.g2d.*;
+import arc.graphics.gl.*;
+import arc.math.*;
 import arc.util.*;
+import mindustry.*;
+import mindustry.game.EventType.*;
+import mindustry.graphics.*;
 import mindustry.io.SaveFileReader.*;
 import mindustry.io.*;
 
@@ -20,6 +27,42 @@ public class OlRenderer{
     public static class DarknessChunk implements CustomChunk {
         public byte[] darkness;
 
+        public DarknessChunk() {
+            // apparently for some unknown reason this is called before the class even reads so, delay time
+            Events.on(WorldLoadEvent.class, event -> {
+                // TODO prefferably not delay this
+                Time.runTask(10f, this::updatePaintedDarkness);
+            });
+        }
+
+        public void initDarknessMap() {
+            darkness = new byte[Vars.world.width() * Vars.world.height()];
+        }
+
+        public void clearDarknessMap() {
+            darkness = null;
+        }
+
+        private void updatePaintedDarkness() {
+            FrameBuffer dark = Reflect.get(BlockRenderer.class, Vars.renderer.blocks, "dark");
+
+            if (darkness != null) {
+                dark.begin();
+
+                int wWidth = dark.getWidth();
+                int wHeight = dark.getHeight();
+
+                Draw.proj().setOrtho(0, 0, dark.getWidth(), dark.getHeight());
+                for (int i = 0; i < darkness.length; i++) {
+                    float alpha = Byte.toUnsignedInt(darkness[i]) / 255f;
+                    if (Mathf.zero(alpha)) continue;
+                    Draw.colorl(1 - alpha);
+                    Fill.rect(i % wWidth + 0.5f, Mathf.floor(i / wWidth) + 0.5f, 1, 1);
+                }
+                dark.end();
+            }
+        }
+
         @Override
         public void read(DataInput stream) throws IOException {
             int len = stream.readInt();
@@ -30,7 +73,6 @@ public class OlRenderer{
                 for (int i = 0; i < len; i++) {
                     byte read = stream.readByte();
 
-                    Log.info("read @", read);
                     darkness[i] = read;
                 }
             } else darkness = null;
