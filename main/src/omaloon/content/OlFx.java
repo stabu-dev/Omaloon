31411be431ea94ec6b811570df9cb599398173cd
Liz <j4224253@gmail.com>
2026-03-18 23:43:07 +0200

    untested, make it crosses

--- main/src/omaloon/content/OlFx.java
+++ main/src/omaloon/content/OlFx.java
@@ -48,13 +48,15 @@ public class OlFx{
             Fill.circle(e.x, e.y, e.rotation * p);
         }
 
-        Draw.alpha(0.5f * Interp.exp5.apply(e.fslope()));
+        Draw.alpha(Interp.exp5.apply(e.fslope()));
         rand.setSeed(e.id + 8);
         for(int i = 0; i < 6; i++) {
             int finalI = i;
             Angles.randLenVectors(e.id + i, 6, e.rotation, e.fin(Interp.smooth) * 360 * (i + 1) * 2 / 6, 360, (x, y) -> {
                 Physics.parallax(vec.set(e.x + x, e.y + y), (finalI + 1) / 6f * e.finpowdown());
-                Fill.circle(vec.x, vec.y, rand.random(0.5f, 3f) * Interp.exp5.apply(e.fslope()));
+                float size = rand.random(0.5f, 3f);
+
+                Fill.rect(vec.x, vec.y, size, size * 3);
             });
         }
     }),
