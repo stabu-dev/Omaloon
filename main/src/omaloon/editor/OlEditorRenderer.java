bc28053f09f9e06b3efbda551c732597b43f925f
Liz <j4224253@gmail.com>
2026-04-01 06:09:06 +0300

    crash

--- main/src/omaloon/editor/OlEditorRenderer.java
+++ main/src/omaloon/editor/OlEditorRenderer.java
@@ -42,7 +42,7 @@ public class OlEditorRenderer extends EditorRenderer{
 
     @Override
     public void draw(float tx, float ty, float tw, float th){
-        arc.struct.IntSeq recaches = Reflect.get(EditorRenderer.class, this, "recacheChunks");
+        arc.struct.IntSet recaches = Reflect.get(EditorRenderer.class, this, "recacheChunks");
         boolean doUpdate = Reflect.<Integer>get(EditorRenderer.class, this, "width") != world.width()
         || Reflect.<Integer>get(EditorRenderer.class, this, "height") != world.height()
         || recaches.size > 0;
