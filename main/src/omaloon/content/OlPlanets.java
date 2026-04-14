e714f0595100b948500e7ff365e8f3e3c5efa776
Liz <j4224253@gmail.com>
2026-04-14 22:02:49 +0300

    make it so that one mesh loader goes for both

--- main/src/omaloon/content/OlPlanets.java
+++ main/src/omaloon/content/OlPlanets.java
@@ -38,9 +38,9 @@ public class OlPlanets{
         glasmore = new OlPlanet("glasmore", omaloon, 1f, 3){{
             generator = new GlasmorePlanetGenerator();
 
-            Prov<GenericMesh> atmosphereMeshLoader = () -> new MultiMesh(
+            Func<Boolean, GenericMesh> atmosphereMeshLoader = isAtmosphere -> new MultiMesh(
                 new NoiseMesh(this, 0, 6, Color.valueOf("d4f2ff").mul(0.8f), 1, 1, 1, 4, 0.025f) {{
-                    shader = OlShaders.depth;
+                    if (isAtmosphere) shader = OlShaders.depth;
                 }},
                 new HeightMesh(this, 6, 0.85f, position -> {
                     int seed = 3;
@@ -63,34 +63,13 @@ public class OlPlanets{
                     if (height > 1.5f) return Color.valueOf("D4F2FF");
                     return Color.valueOf("4F3F3B");
                 }) {{
-                    shader = OlShaders.depth;
+                    if (isAtmosphere) shader = OlShaders.depth;
                 }}
             );
 
             meshLoader = () -> new MultiMesh(
-                new AtmosphereMesh(this, atmosphereMeshLoader.get()),
-                new NoiseMesh(this, 0, 6, Color.valueOf("d4f2ff").mul(0.8f), 1, 1, 1, 4, 0.025f),
-                new HeightMesh(this, 6, 0.85f, position -> {
-                    int seed = 3;
-                    double octaves = 7, persistence = 0.7, scale = 0.25;
-                    float mag = 2;
-
-                    float powMountain = Mathf.clamp(Mathf.pow(Simplex.noise3d(
-                        7 + seed, octaves, persistence, scale,
-                        5 + position.x, 5 + position.y, 5 + position.z
-                    ), 12f) * 300f, 0, 0.5f);
-
-                    return Simplex.noise3d(
-                        7 + seed, octaves, persistence, scale,
-                        5 + position.x, 5 + position.y, 5 + position.z
-                    ) * mag + powMountain;
-
-                }, (position, height) -> {
-                    if (height < 1f) return Color.valueOf("574F51");
-
-                    if (height > 1.5f) return Color.valueOf("D4F2FF");
-                    return Color.valueOf("4F3F3B");
-                })
+                new AtmosphereMesh(this, atmosphereMeshLoader.get(true)),
+                atmosphereMeshLoader.get(false)
             );
             cloudMeshLoader = () -> new MultiMesh(
                 new HexSkyMesh(this, 6, -0.5f, 0.14f, 6, Color.valueOf("D4F2FF").a(0.3f), 2, 0.42f, 1f, 0.6f),
