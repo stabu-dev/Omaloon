2227e3ffa061e1a3fc0ba8c3bc962ec59fc716f7
stabu-dev <buksta@outlook.com>
2026-04-24 18:07:58 +0300

    purpura is back :D

--- main/src/omaloon/content/OlPlanets.java
+++ main/src/omaloon/content/OlPlanets.java
@@ -7,7 +7,6 @@ import arc.util.noise.*;
 import mindustry.content.*;
 import mindustry.graphics.g3d.*;
 import mindustry.type.*;
-import mindustry.ui.dialogs.*;
 import mindustry.world.meta.*;
 import omaloon.content.blocks.*;
 import omaloon.graphics.*;
@@ -19,21 +18,20 @@ public class OlPlanets{
     public static Planet
     omaloon,
     asteroidBelt,
-
-    glasmore;
+    glasmore, purpura;
 
     public static void load(){
-        omaloon = new Planet("omaloon", null, 4f){{
+        omaloon = new Planet("omaloon", null, 3f){{
             bloom = true;
             accessible = false;
             meshLoader = () -> new SunMesh(
-                this, 4, 5, 0.3f, 1.0f, 1.2f, 1, 1.3f,
-                Color.valueOf("#8B4513"),
-                Color.valueOf("#A0522D"),
-                Color.valueOf("c2311e"),
-                Color.valueOf("ff6730"),
-                Color.valueOf("bf342f"),
-                Color.valueOf("8e261d")
+            this, 4, 5, 0.3f, 1.0f, 1.2f, 1, 1.3f,
+            Color.valueOf("#8B4513"),
+            Color.valueOf("#A0522D"),
+            Color.valueOf("c2311e"),
+            Color.valueOf("ff6730"),
+            Color.valueOf("bf342f"),
+            Color.valueOf("8e261d")
             );
         }};
 
@@ -50,80 +48,78 @@ public class OlPlanets{
             clipRadius = 30f;
 
             meshLoader = () -> new AsteroidBeltMesh(this,
-                60,
-                20f,
-                6f,
-                1.5f,
-                0.01f, 0.08f,
-                42,
-                Blocks.stoneWall,
-                OlEnvironmentBlocks.verdantAghaniteWall,
-                0.5f
+            60,
+            30f,
+            6f,
+            1.5f,
+            0.01f, 0.08f,
+            42,
+            Blocks.stoneWall,
+            OlEnvironmentBlocks.verdantAghaniteWall,
+            0.5f
             );
         }};
 
         glasmore = new OlPlanet("glasmore", omaloon, 1f, 3){{
             generator = new GlasmorePlanetGenerator();
 
-            Func<Boolean, GenericMesh> atmosphereMeshLoader = isAtmosphere -> {
-                return new MultiMesh(
-                    new NoiseMesh(this, 0, 6, Color.valueOf("d4f2ff").mul(0.8f), 1, 1, 1, 4, 0.025f) {{
-                        if (isAtmosphere) shader = OlShaders.depth;
-                    }},
-                    new HeightMesh(this, 6, 0.85f, position -> {
-                        int seed = 3;
-                        double octaves = 7, persistence = 0.7, scale = 0.25;
-                        float mag = 2;
+            Func<Boolean, GenericMesh> atmosphereMeshLoader = isAtmosphere -> new MultiMesh(
+            new NoiseMesh(this, 0, 6, Color.valueOf("d4f2ff").mul(0.8f), 1, 1, 1, 4, 0.025f){{
+                if(isAtmosphere) shader = OlShaders.depth;
+            }},
+            new HeightMesh(this, 6, 0.85f, position -> {
+                int seed = 3;
+                double octaves = 7, persistence = 0.7, scale = 0.25;
+                float mag = 2;
 
-                        float powMountain = Mathf.clamp(Mathf.pow(Simplex.noise3d(
-                            7 + seed, octaves, persistence, scale,
-                            5 + position.x, 5 + position.y, 5 + position.z
-                        ), 12f) * 300f, 0, 0.5f);
+                float powMountain = Mathf.clamp(Mathf.pow(Simplex.noise3d(
+                7 + seed, octaves, persistence, scale,
+                5 + position.x, 5 + position.y, 5 + position.z
+                ), 12f) * 300f, 0, 0.5f);
 
-                        return Simplex.noise3d(
-                            7 + seed, octaves, persistence, scale,
-                            5 + position.x, 5 + position.y, 5 + position.z
-                        ) * mag + powMountain;
+                return Simplex.noise3d(
+                7 + seed, octaves, persistence, scale,
+                5 + position.x, 5 + position.y, 5 + position.z
+                ) * mag + powMountain;
 
-                    }, (position, height) -> {
-                        if (height < 1f) return Color.valueOf("574F51");
+            }, (position, height) -> {
+                if(height < 1f) return Color.valueOf("574F51");
 
-                        if (height > 1.5f) return Color.valueOf("D4F2FF");
-                        return Color.valueOf("4F3F3B");
-                    }) {{
-                        if (isAtmosphere) shader = OlShaders.depth;
-                    }},
-                    new HeightMesh(this, 6, 0.85f, position -> {
-                        int seed = 5;
-                        double octaves = 5, persistence = 0.7, scale = 0.4;
-                        float mag = 1.75f;
+                if(height > 1.5f) return Color.valueOf("D4F2FF");
+                return Color.valueOf("4F3F3B");
+            }){{
+                if(isAtmosphere) shader = OlShaders.depth;
+            }},
+            new HeightMesh(this, 6, 0.85f, position -> {
+                int seed = 5;
+                double octaves = 5, persistence = 0.7, scale = 0.4;
+                float mag = 1.75f;
 
-                        return new Interp.Pow(10).apply(Simplex.noise3d(
-                            7 + seed, octaves, persistence, scale,
-                            5 + position.x, 5 + position.y, 5 + position.z
-                        )) * mag * Simplex.noise3d(
-                            17 + seed, 3, 0.7, 0.5,
-                            5 + position.x, 5 + position.y, 5 + position.z
-                        );
-
-                    }, (position, height) -> Color.valueOf("4F3F3B")) {{
-                        if (isAtmosphere) shader = OlShaders.depth;
-                    }}
+                return new Interp.Pow(10).apply(Simplex.noise3d(
+                7 + seed, octaves, persistence, scale,
+                5 + position.x, 5 + position.y, 5 + position.z
+                )) * mag * Simplex.noise3d(
+                17 + seed, 3, 0.7, 0.5,
+                5 + position.x, 5 + position.y, 5 + position.z
                 );
-            };
+
+            }, (position, height) -> Color.valueOf("4F3F3B")){{
+                if(isAtmosphere) shader = OlShaders.depth;
+            }}
+            );
 
             meshLoader = () -> new MultiMesh(
-                new AtmosphereMesh(this, atmosphereMeshLoader.get(true)),
-                atmosphereMeshLoader.get(false),
-                new QuadMesh(this, "omaloon-rings"){{
-                    radius = 2.4f;
-                    stroke = 1f;
-                    updateMesh();
-                }}
+            new AtmosphereMesh(this, atmosphereMeshLoader.get(true)),
+            atmosphereMeshLoader.get(false),
+            new QuadMesh(this, "omaloon-rings"){{
+                radius = 2.4f;
+                stroke = 1f;
+                updateMesh();
+            }}
             );
             cloudMeshLoader = () -> new MultiMesh(
-                new HexSkyMesh(this, 6, -0.5f, 0.14f, 6, Color.valueOf("D4F2FF").a(0.3f), 2, 0.42f, 1f, 0.6f),
-                new HexSkyMesh(this, 1, 0.6f, 0.15f, 6, Color.valueOf("D4F2FF").a(0.3f), 2, 0.42f, 1.2f, 0.5f)
+            new HexSkyMesh(this, 6, -0.5f, 0.14f, 6, Color.valueOf("D4F2FF").a(0.3f), 2, 0.42f, 1f, 0.6f),
+            new HexSkyMesh(this, 1, 0.6f, 0.15f, 6, Color.valueOf("D4F2FF").a(0.3f), 2, 0.42f, 1.2f, 0.5f)
             );
             loadIcon = false;
             alwaysUnlocked = true;
@@ -131,10 +127,10 @@ public class OlPlanets{
             hasAtmosphere = true;
             atmosphereRadIn = 0.02f;
             atmosphereRadOut = 0.3f;
-            orbitRadius = 40f;
-//            totalRadius += 2.6f;
-//            lightSrcTo = 0.5f;
-//            lightDstFrom = 0.2f;
+            orbitRadius = 20f;
+//          totalRadius += 2.6f;
+//          lightSrcTo = 0.5f;
+//          lightDstFrom = 0.2f;
             clearSectorOnLose = true;
             defaultCore = Blocks.coreBastion;
             iconColor = Color.valueOf("5e929d");
@@ -147,6 +143,28 @@ public class OlPlanets{
             campaignRuleDefaults.showSpawns = true;
 
             unlockedOnLand.add(OlStorageBlocks.landingCapsule);
+        }};
+
+        purpura = new OlPlanet("purpura", omaloon, 1.5f, 0){{
+            icon = "purpura";
+            accessible = false;
+
+            atmosphereColor = Color.valueOf("4F424D");
+            atmosphereRadIn = 0;
+            atmosphereRadOut = 0.05f;
+            orbitRadius = 50f;
+            generator = new PurpuraPlanetGenerator();
+
+            meshLoader = () -> new MultiMesh(
+            new AtmosphereHexMesh(7),
+            new HexMesh(this, 7)
+            );
+            cloudMeshLoader = () -> new MultiMesh(
+            new HexSkyMesh(this, 1, 1f, 0.05f, 6, Color.valueOf("242424").a(0.6f), 2, 0.8f, 1f, 0.f),
+            new HexSkyMesh(this, 2, -1.3f, 0.06f, 6, Color.valueOf("413B42").a(0.6f), 2, 0.8f, 1f, 0.5f),
+            new HexSkyMesh(this, 3, 1.3f, 0.07f, 6, Color.valueOf("7F777E").a(0.6f), 2, 0.8f, 1.2f, 0.5f),
+            new HexSkyMesh(this, 4, -1.6f, 0.08f, 6, Color.valueOf("B2B2B2").a(0.6f), 2, 0.8f, 1.2f, 0.5f)
+            );
         }};
     }
 }
