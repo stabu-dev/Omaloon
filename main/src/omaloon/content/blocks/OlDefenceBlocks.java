712c439a42dc7883f0870a2ad645e55547a73b27
stabu-dev <buksta@outlook.com>
2026-06-08 21:32:59 +0300

    its unnoticeable anyway

--- main/src/omaloon/content/blocks/OlDefenceBlocks.java
+++ main/src/omaloon/content/blocks/OlDefenceBlocks.java
@@ -245,7 +245,6 @@ public class OlDefenceBlocks{
             researchCostMultiplier = 0.1f;
         }};
         compositeWallLarge = new Wall("composite-wall-large"){{
-            //TODO fix the icon being funny
             requirements(Category.defense, mult(compositeWall.requirements, 4f));
             health = 90 * 4 * wallHealthMultiplier;
             size = 2;
